6232d4575a3d918f50e7b8ecce4c6ce3fb870acf
AldoRuiz <aldopablo7@hotmail.com>
2023-08-28 22:43:47 -0300

    Se hizo una pequeña correcion en los archivos .html y .js

--- Sprint 2/index.js
+++ Sprint 2/index.js
@@ -22,17 +22,20 @@ function viewCard(data, container){
 
 
 function createCard(objeto){
-    return `<div class="card text-bg-dark text-center" style="width: auto;" >
-    <img src="${objeto.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">${objeto.name}</h5>
-        <p class="card-text">
-            ${objeto.description}
-        </p>
-    </div>
-    <div class="card-footer text-end d-flex justify-content-lg-between justify-content-between border-top">
-        <span>$ ${objeto.price}</span>
-        <a href="./assets/pages/details.html" class="btn btn-primary ">Details</a>                        
-    </div>
-</div>`
+    return `
+    <div class="col-sm-12 col-lg-3 col-md-6 mb-3 mt-3 mb-sm-4">
+        <div class="card text-bg-dark text-center" style="width: auto;" >
+            <img src="${objeto.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${objeto.name}</h5>
+                <p class="card-text">
+                ${objeto.description}
+                </p>
+        </div>
+        <div class="card-footer text-end d-flex justify-content-lg-between justify-content-between border-top">
+            <span>$ ${objeto.price}</span>
+            <a href="./assets/pages/details.html" class="btn btn-primary ">Details</a>                        
+        </div>
+        </div>
+    </div>`
 }
